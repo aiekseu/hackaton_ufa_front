ea4b47b403c38f5eea673e1eb0e4c36778f1d818
Belig Chimitov <belig.ch111@gmail.com>
2021-03-21 18:57:55 +0700

    file

--- src/components/DeploymentGraph/Cubes.js
+++ src/components/DeploymentGraph/Cubes.js
@@ -3,7 +3,7 @@ import {Grid, Paper, Typography} from "@material-ui/core";
 import {makeStyles} from '@material-ui/core/styles';
 import lightGreen from '@material-ui/core/colors/lightGreen';
 import ArrowUpwardRoundedIcon from '@material-ui/icons/ArrowUpwardRounded';
-import StockChart from '../StockChart';
+import StockChart from './StockChart';
 
 const color = lightGreen["A400"];
 
